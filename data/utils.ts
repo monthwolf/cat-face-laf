0923454d53f4c8bfa6e2bd162ebb74f26ff17388
monthwolf <52775320+monthwolf@users.noreply.github.com>
2024-10-15 20:34:52 +0800

    Update utils.ts

--- data/utils.ts
+++ data/utils.ts
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import request from "request";
 
+import { S3 } from "@aws-sdk/client-s3";
 import { Cloud } from "laf-client-sdk";
-import COS from 'cos-nodejs-sdk-v5';
+// import COS from 'cos-nodejs-sdk-v5';
+
 import { config } from "./config";
 
 export function stdlog(content: string, color: string = 'default') {
@@ -23,91 +25,87 @@ export function stdlog(content: string, color: string = 'default') {
     process.stdout.write(`${colorsDict[color]}${content}\x1B[0m`);
 }
 
-export function getCos() {
-    var cos = new COS({
-        getAuthorization: async function (options: Object, callback: Function) {
-            // 初始化时不会调用，只有调用 cos 方法（例如 cos.putObject）时才会进入
-            var cosTemp = config.COS_KEY;
-            if (!cosTemp || !cosTemp.Credentials) {
-                console.error("无效cosTemp信息: ", cosTemp)
-                callback({
-                    TmpSecretId: "empty",
-                    TmpSecretKey: "empty",
-                    SecurityToken: "empty",
-                    ExpiredTime: "1111111111",
-                });
-                return;
-            }
+export async function getCos() {
+    // var cos = new COS({
+    //     Domain:"https://oss.laf.run/{Bucket}",
+    //     getAuthorization: async function (options: Object, callback: Function) {
+    //         // 初始化时不会调用，只有调用 cos 方法（例如 cos.putObject）时才会进入
+    //         var cosTemp = config.COS_KEY;
+    //         if (!cosTemp || !cosTemp.Credentials) {
+    //             console.error("无效cosTemp信息: ", cosTemp)
+    //             callback({
+    //                 TmpSecretId: "empty",
+    //                 TmpSecretKey: "empty",
+    //                 SecurityToken: "empty",
+    //                 ExpiredTime: "1111111111",
+    //             });
+    //             return;
+    //         }
+    //
+    //         callback({
+    //             TmpSecretId: cosTemp.Credentials.TmpSecretId,        // 临时密钥的 tmpSecretId
+    //             TmpSecretKey: cosTemp.Credentials.TmpSecretKey,      // 临时密钥的 tmpSecretKey
+    //             SecurityToken: cosTemp.Credentials.Token,            // 临时密钥的 sessionToken
+    //             ExpiredTime: cosTemp.ExpiredTime,                    // 临时密钥失效时间戳，是申请临时密钥时，时间戳加 durationSeconds
+    //         });
+    //     }
+    // });
+    //
+    // return cos;
 
-            callback({
-                TmpSecretId: cosTemp.Credentials.TmpSecretId,        // 临时密钥的 tmpSecretId
-                TmpSecretKey: cosTemp.Credentials.TmpSecretKey,      // 临时密钥的 tmpSecretKey
-                SecurityToken: cosTemp.Credentials.Token,            // 临时密钥的 sessionToken
-                ExpiredTime: cosTemp.ExpiredTime,                    // 临时密钥失效时间戳，是申请临时密钥时，时间戳加 durationSeconds
-            });
-        }
+
+// @ts-ignore
+    const cloud = new Cloud({
+  baseUrl: `https://${config.LAF_APPID}.laf.run`
+});
+// 获取云存储临时令
+const { credentials, endpoint, region } =await cloud.invokeFunction(
+  "get-oss-sts",
+  {}
+);
+    //stdlog(JSON.stringify(res),"green")
+    const s3 = new S3({
+      endpoint: endpoint,
+      region: region,
+      credentials: {
+        accessKeyId: credentials.AccessKeyId,
+        secretAccessKey: credentials.SecretAccessKey,
+        sessionToken: credentials.SessionToken,
+        expiration: credentials.Expiration,
+      },
+      forcePathStyle: true,
     });
-
-    return cos;
+    //stdlog(String(s3),"green")
+    return s3;
 }
 
 
-export function downloadCosPath(cos: COS, path: string, localPath: string) {
-    const pathObj = _getRegionBucketPath(path);
-    return new Promise((resolve, reject) => {
-        cos.getObjectUrl(
-            {
-                Bucket: pathObj.bucket, /* 填入您自己的存储桶，必须字段 */
-                Region: pathObj.region, /* 存储桶所在地域，例如 ap-beijing，必须字段 */
-                Key: pathObj.filePath, /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），支持中文，必须字段 */
-                Sign: true,
-            },
-            function (err, data) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
 
-                var writeStream = fs.createWriteStream(localPath);
-                request(data.Url).on('response', (response) => {
-                        if (!/^image\//.test(response.headers['content-type'] as string)) {
-                            writeStream.close();
-                            fs.unlinkSync(localPath);
-                            reject(new Error('Download failed: Unauthorized'));
-                        }
-                    })
-                    .pipe(writeStream)
-                    .on('finish', resolve)
-                    .on('error', (error) => {
-                        writeStream.close();
-                        fs.unlinkSync(localPath);
-                        reject(error);
-                    });
+
+export async function downloadCosPath(cos: S3, path: string, localPath: string) {
+    // const pathObj = _getRegionBucketPath(path);
+        // bucket name prefixed with appid
+    // const cmd = new GetObjectCommand({
+    //   Bucket: pathObj.bucket,
+    //   Key: pathObj.filePath,
+    // });
+    return new Promise((resolve, reject) => {
+        var writeStream = fs.createWriteStream(localPath);
+        path=encodeURI(path)
+        request(path).on('response', (response) => {
+            if (!/^image\//.test(response.headers['content-type'] as string)) {
+                writeStream.close();
+                fs.unlinkSync(localPath);
+                reject(new Error('Download failed: Unauthorized'));
             }
-        );
+        }).pipe(writeStream)
+            .on('finish', resolve)
+            .on('error', (error) => {
+                writeStream.close();
+                fs.unlinkSync(localPath);
+                reject(error);
+            });
     })
 }
 
 
-function _splitOnce(str: string, sep: string) {
-    const idx = str.indexOf(sep);
-    return [str.slice(0, idx), str.slice(idx + 1)];
-}
-
-// 提取COS的region和bucket字段
-function _getRegionBucketPath(url: string) {
-    // 返回：{region: 'ap-guangzhou', bucket: 'bucket-name', filePath: "xxx/xxx.xxx"}
-    const regex = /http[s]*:\/\//i;
-    const newUrl = url.replace(regex, '');
-    const items = _splitOnce(newUrl, '/');
-    const firstItems = items[0].split('.');
-
-    if (firstItems[0] !== 'cos') {
-        // 例如：https://bucket-name.cos.ap-guangzhou.myqcloud.com/sample.png
-        return { region: firstItems[2], bucket: firstItems[0], filePath: items[1] }
-    }
-
-    // 例如：https://cos.ap-guangzhou.myqcloud.com/bucket-name/sample.png
-    const path = _splitOnce(items[1], '/');
-    return { region: firstItems[1], bucket: path[0], filePath: path[1] }
-}
